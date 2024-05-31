aceac9e5aedc29a3ad8d2c1a6ba6b03e12bf9637
Aarnav Srivastava <70161949+AarnavSrivastava@users.noreply.github.com>
2024-05-31 10:55:07 -0400

    msg

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,8 +35,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossOrigin="anonymous" />
-      <body className={`${inter.variable} ${playfair_display.variable} ${dosis.variable} ${nunito_sans.variable} bg-background w-screen`}>{children}</body>
+      <body className={`${inter.variable} ${playfair_display.variable} ${dosis.variable} ${nunito_sans.variable} bg-background w-screen`}>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossOrigin="anonymous" />
+        {children}
+      </body>
     </html>
   );
 }
